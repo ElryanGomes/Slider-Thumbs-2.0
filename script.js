82483058071bd093f7ac0ea7c561756453a1da99
Elryan Gomes <elryangomes@gmail.com>
2025-01-19 09:38:36 -0300

    slider-thumbs-2.0

---
+++ script.js
@@ -0,0 +1,31 @@
+let btnNext = document.querySelector('.next')
+let btnBack = document.querySelector('.back')
+
+let list = document.querySelector('.container .list')
+let thumbs = document.querySelector('.container .thumbs')
+let container = document.querySelector('.container')
+
+btnNext.onclick = () => moveCardOnClick('next')
+btnBack.onclick = () => moveCardOnClick('back')
+
+function moveCardOnClick(type) {
+    let listItens = document.querySelectorAll('.list-item')
+    let thumbItens = document.querySelectorAll('.thumb-items')
+
+    // console.log(thumbItens)
+
+    if (type === 'next') {
+        list.appendChild(listItens[0])
+        thumbs.appendChild(thumbItens[0])
+        container.classList.add('next')
+    }
+    else {
+        list.prepend(listItens[listItens.length - 1])
+        thumbs.prepend(thumbItens[thumbItens.length - 1])
+        container.classList.add('back')
+    }
+    setTimeout(() => {
+        container.classList.remove("next");
+        container.classList.remove("back");
+      }, 500);
+}
